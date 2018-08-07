e93a13a96726e128f74fba1f5233c9871d35a3e1
PawelSzyb <pawelszybaj@@wp.pl>
2018-08-07 20:48:06 +0200

    prepare for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://Pawel:pawel1@ds161411.mlab.com:61411/contact-list"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const contacts = require("./routes/api/contacts");
 
@@ -21,6 +22,16 @@ mongoose
 
 // use routes
 app.use("/api/contacts", contacts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(__dirname, "client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 

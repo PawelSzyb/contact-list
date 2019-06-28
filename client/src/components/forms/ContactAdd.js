7c08109b714d9e23968ae2787d4871defacb93eb
Paweł Szybaj <pawelszybaj@wp.pl>
2019-06-28 20:03:32 +0200

    working with hooks and formik

--- client/src/components/forms/ContactAdd.js
+++ client/src/components/forms/ContactAdd.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { Row, Input, Button, Icon, Col, Container } from "react-materialize";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { Row, Input, Button, Icon, Col, Container } from 'react-materialize';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import { addContact } from "../../actions/contactActions";
+import { addContact } from '../../actions/contactActions';
 
-import "../../App.css";
+import '../../App.css';
 
 class ContactAdd extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: "",
-      email: "",
-      number: "",
+      name: '',
+      email: '',
+      number: '',
       errors: {}
     };
     this.onChange = this.onChange.bind(this);
@@ -47,48 +47,48 @@ class ContactAdd extends Component {
     return (
       <Container>
         <Row
-          style={{ display: "flex", justifyContent: "center", float: "none" }}
+          style={{ display: 'flex', justifyContent: 'center', float: 'none' }}
         >
-          <Col s={12} m={8} l={6} style={{ margin: "20px auto" }}>
+          <Col s={12} m={8} l={6} style={{ margin: '20px auto' }}>
             <h3>
-              <span style={{ color: "rgb(183, 28, 28)" }}>Add </span> New
+              <span style={{ color: 'rgb(183, 28, 28)' }}>Add </span> New
               Contact
             </h3>
             <Input
               s={12}
-              name="name"
+              name='name'
               onChange={this.onChange}
               value={this.state.name}
               error={errors.name ? errors.name : null}
-              placeholder=""
-              label="Name"
+              placeholder=''
+              label='Name'
             />
             <Input
               s={12}
-              name="email"
-              type="email"
-              label="Email"
-              placeholder=""
+              name='email'
+              type='email'
+              label='Email'
+              placeholder=''
               onChange={this.onChange}
               value={this.state.email}
               error={errors.email ? errors.email : null}
             />
             <Input
-              type="text"
-              name="number"
-              placeholder=""
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              label="Number"
+              type='text'
+              name='number'
+              placeholder=''
+              pattern='[0-9]{3}[0-9]{3}[0-9]{3}'
+              label='Number'
               s={12}
               onChange={this.onChange}
               value={this.state.number}
               error={errors.number ? errors.number : null}
             />
             <Button
-              type="submit"
+              type='submit'
               onClick={this.onSubmitClick}
-              waves="light"
-              style={{ backgroundColor: "#b71c1c", marginTop: "20px" }}
+              waves='light'
+              style={{ backgroundColor: '#b71c1c', marginTop: '20px' }}
             >
               Add
               <Icon right>add</Icon>
@@ -105,7 +105,7 @@ ContactAdd.propTypes = {
   errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   errors: state.errors,
   credentials: state.credentials
 });
--- client/src/components/forms/EditForm.js
+++ client/src/components/forms/EditForm.js
@@ -1,43 +1,40 @@
-import React, { Component } from "react";
-import { Row, Input, Button, Icon, Col, Container } from "react-materialize";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { Component, Fragment } from 'react';
+import { Row, Input, Button, Icon, Col, Container } from 'react-materialize';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Formik, Form } from 'formik';
 
-import isEmpty from "../is-empty/IsEmpty";
+import isEmpty from '../is-empty/IsEmpty';
 
 import {
   updateContact,
   getContacts,
   clearErrors
-} from "../../actions/contactActions";
+} from '../../actions/contactActions';
 
 class EditForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      name: "",
-      email: "",
-      number: "",
+      name: '',
+      email: '',
+      number: '',
       errors: {}
     };
-    this.onChange = this.onChange.bind(this);
-    this.onSubmitClick = this.onSubmitClick.bind(this);
-  }
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
   }
 
-  onSubmitClick() {
+  onSubmitHandle = (values, actions) => {
     const contactData = {
-      name: this.state.name,
-      email: this.state.email,
-      number: this.state.number
+      name: values.name,
+      email: values.email,
+      number: values.number
     };
     const { id } = this.props.match.params;
     this.props.updateContact(id, contactData, this.props.history);
-  }
+    actions.setSubmitting(false);
+  };
 
   componentDidMount() {
     this.props.getContacts();
@@ -51,11 +48,11 @@ class EditForm extends Component {
     if (nextProps.contacts.contacts) {
       const { id } = this.props.match.params;
       const { contacts } = nextProps.contacts;
-      const contact = contacts.filter(contact => contact._id === id);
+      const contact = contacts.filter((contact) => contact._id === id);
 
-      contact[0].name = !isEmpty(contact[0].name) ? contact[0].name : "";
-      contact[0].email = !isEmpty(contact[0].email) ? contact[0].email : "";
-      contact[0].number = !isEmpty(contact[0].number) ? contact[0].number : "";
+      contact[0].name = !isEmpty(contact[0].name) ? contact[0].name : '';
+      contact[0].email = !isEmpty(contact[0].email) ? contact[0].email : '';
+      contact[0].number = !isEmpty(contact[0].number) ? contact[0].number : '';
       this.setState({
         name: contact[0].name,
         email: contact[0].email,
@@ -69,52 +66,69 @@ class EditForm extends Component {
     return (
       <Container>
         <Row
-          style={{ display: "flex", justifyContent: "center", float: "none" }}
+          style={{ display: 'flex', justifyContent: 'center', float: 'none' }}
         >
-          <Col s={12} m={8} l={6} style={{ margin: "20px auto" }}>
+          <Col s={12} m={8} l={6} style={{ margin: '20px auto' }}>
             <h3>
-              <span style={{ color: "rgb(183, 28, 28)" }}>Edit </span>
+              <span style={{ color: 'rgb(183, 28, 28)' }}>Edit </span>
               Contact
             </h3>
-            <Input
-              s={12}
-              name="name"
-              onChange={this.onChange}
-              value={this.state.name}
-              error={errors.name ? errors.name : null}
-              placeholder="Name"
-              label="Name"
-            />
-            <Input
-              s={12}
-              name="email"
-              type="email"
-              label="Email"
-              placeholder="Email"
-              onChange={this.onChange}
-              value={this.state.email}
-              error={errors.email ? errors.email : null}
-            />
-            <Input
-              type="text"
-              name="number"
-              placeholder="Number"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-              label="Number"
-              s={12}
-              onChange={this.onChange}
-              value={this.state.number}
-              error={errors.number ? errors.number : null}
-            />
-            <Button
-              type="submit"
-              onClick={this.onSubmitClick}
-              waves="light"
-              style={{ backgroundColor: "#b71c1c", marginTop: "20px" }}
+            <Formik
+              initialValues={{
+                name: this.state.name,
+                email: this.state.email,
+                number: this.state.number
+              }}
+              enableReinitialize={true}
+              onSubmit={(values, actions) =>
+                this.onSubmitHandle(values, actions)
+              }
             >
-              Edit
-              <Icon right>mode_edit</Icon>
-            </Button>
+              {({ values, handleChange, handleSubmit }) => (
+                <Fragment>
+                  <Form onSubmit={handleSubmit}>
+                    <Input
+                      s={12}
+                      name='name'
+                      onChange={handleChange}
+                      value={values.name}
+                      error={errors.name ? errors.name : null}
+                      placeholder='Name'
+                      label='Name'
+                    />
+                    <Input
+                      s={12}
+                      name='email'
+                      type='email'
+                      label='Email'
+                      placeholder='Email'
+                      onChange={handleChange}
+                      value={values.email}
+                      error={errors.email ? errors.email : null}
+                    />
+                    <Input
+                      type='text'
+                      name='number'
+                      placeholder='Number'
+                      pattern='[0-9]{3}[0-9]{3}[0-9]{3}'
+                      label='Number'
+                      s={12}
+                      onChange={handleChange}
+                      value={values.number}
+                      error={errors.number ? errors.number : null}
+                    />
+                    <Button
+                      type='submit'
+                      waves='light'
+                      style={{ backgroundColor: '#b71c1c', marginTop: '20px' }}
+                    >
+                      Edit
+                      <Icon right>mode_edit</Icon>
+                    </Button>
+                  </Form>
+                </Fragment>
+              )}
+            </Formik>
           </Col>
         </Row>
       </Container>
@@ -124,11 +138,11 @@ class EditForm extends Component {
 
 EditForm.propTypes = {
   updateContact: PropTypes.func.isRequired,
-  getContacts: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  getContacts: PropTypes.func.isRequired
+  // errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   errors: state.errors,
   contacts: state.contacts
 });
